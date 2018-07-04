179d6cc3aa8dfb275022df41e6265e96bc70b5b2
Anish Mashankar <anishmcloud@gmail.com>
2018-07-04 22:16:42 +0530

    get the installer working

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
 import setuptools
+import os
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="escat",
-    version="0.0.1",
-    author="Anish Mashankar",
-    author_email="anishmcloud@gmail.com",
-    description="Command line wrapper for Elasticsearch CAT API",
+    name='escat',
+    version='0.0.1',
+    author='Anish Mashankar',
+    scripts=["bin/escat"],
+    description='Command line wrapper for Elasticsearch CAT API',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/itisanish/escat",
+    url='https://github.com/itisanish/escat',
     packages=setuptools.find_packages(),
+    python_requires='>=3.0',
+    data_files=[(os.path.join(os.path.expanduser("~"), ".escat"), ["resources/config.yml"])],
     classifiers=(
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ),
+    install_requires=[
+        "elasticsearch",
+        "pyyaml"
+    ]
 )
